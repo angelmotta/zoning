8ed6ae3308f8c0ff83cde2c1dd892a53be067e5d
angelmotta <angelmotta@gmail.com>
2016-07-26 01:02:30 -0500

    Add files via upload

--- forms.py
+++ forms.py
@@ -5,5 +5,5 @@ from wtforms import validators, ValidationError
 class wwnForm(Form):
     host = TextField('Hostname Lpar', [validators.DataRequired("Please Enter the Server Name")])
     vios = SelectField('VIOS', choices=[('viochp11_12', 'VIOCHP11_12'), ('violmp11_12', 'VIOLMP11_12'), ('viochp08_09', 'VIOCHP08_09')])
-    wwn = TextAreaField('Enter your wwns', [validators.Length(min=56, max=56, message="Enter 4 lines with your WWNs")])
+    wwn = TextAreaField('Enter your wwns', [validators.Length(min=54, max=54, message="Enter 4 lines with your WWNs")])
     submit = SubmitField('Zoning')
