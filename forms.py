3cc5ea38ac97430411b0e9e24857ba609710638e
angelmotta <angelmotta@gmail.com>
2016-08-08 21:37:58 -0500

    Add files via upload
    Se mejora el orden como se muestran las zonas
    Se corrigen el formato de las zonas
    Se adiciona la logica para soportar VIOLMP08_09

--- forms.py
+++ forms.py
@@ -4,6 +4,6 @@ from wtforms import validators, ValidationError
 
 class wwnForm(Form):
     host = TextField('Hostname Lpar', [validators.DataRequired("Please Enter the Server Name")])
-    vios = SelectField('VIOS', choices=[('viochp11_12', 'VIOCHP11_12'), ('violmp11_12', 'VIOLMP11_12'), ('viochp08_09', 'VIOCHP08_09')])
-    wwn = TextAreaField('Enter your wwns', [validators.Length(min=54, max=54, message="Enter 4 lines with your WWNs")])
+    vios = SelectField('VIOS', choices=[('viochp11_12', 'VIOCHP11_12'), ('violmp11_12', 'VIOLMP11_12'), ('viochp08_09', 'VIOCHP08_09'), ('violmp08_09', 'VIOLMP08_09')])
+    wwn = TextAreaField('Enter your wwns', [validators.Length(min=64, message="Enter at least 4 lines with your WWNs")])
     submit = SubmitField('Zoning')
--- genzone.py
+++ genzone.py
@@ -6,7 +6,7 @@ def wwnparse(wwn):
     for wwn_lpar in lista_wwn:
         print(wwn_lpar)
         #wwn_lpar = line.rstrip() 
-        wwn = wwn_lpar[0:2]+":"+wwn_lpar[2:4]+":"+wwn_lpar[4:6]+":"+wwn_lpar[6:8]+":"+wwn_lpar[8:10]+":"+wwn_lpar[10:12]
+        wwn = wwn_lpar[0:2]+":"+wwn_lpar[2:4]+":"+wwn_lpar[4:6]+":"+wwn_lpar[6:8]+":"+wwn_lpar[8:10]+":"+wwn_lpar[10:12]+":"+wwn_lpar[12:14]+":"+wwn_lpar[14:16]
         lista_wwn_result.append(wwn)
     wwn1 = lista_wwn_result[0]
     wwn2 = lista_wwn_result[1]
@@ -34,6 +34,12 @@ def lpar_alias(hostname, vios):
         lpar_alias3 = "VIOCHP09_" + hostname + "_FCSV0"
         lpar_alias4 = "VIOCHP09_" + hostname + "_FCSV1"
         return lpar_alias1, lpar_alias2, lpar_alias3, lpar_alias4
+    if vios == 'violmp08_09':
+        lpar_alias1 = "VIOLMP08_" + hostname + "_FCSV0"
+        lpar_alias2 = "VIOLMP08_" + hostname + "_FCSV1"
+        lpar_alias3 = "VIOLMP09_" + hostname + "_FCSV0"
+        lpar_alias4 = "VIOLMP09_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2, lpar_alias3, lpar_alias4
 
 def zonename1(lpar_alias):
     if "VIOCHP11_" in lpar_alias:
@@ -48,6 +54,11 @@ def zonename1(lpar_alias):
         return zone_vios1_fab1,vsp_port_vios1_1,fabric1
     elif "VIOCHP08_" in lpar_alias:
         vsp_port_vios1_1 = "VSP6456_9A"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "VIOLMP08_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_9A"
         zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
         fabric1 = "P_Fabric1"
         return zone_vios1_fab1,vsp_port_vios1_1,fabric1
@@ -68,6 +79,11 @@ def zonename2(lpar_alias):
         zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
         fabric2 = "P_Fabric2"
         return zone_vios1_fab2,vsp_port_vios1_2,fabric2            
+    elif "VIOLMP08_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6547_2C"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2            
 
 def zonename3(lpar_alias):    
     if "VIOCHP12_" in lpar_alias:
@@ -80,6 +96,10 @@ def zonename3(lpar_alias):
         return zone_vios2_fab1,vsp_port_vios2_1
     elif "VIOCHP09_" in lpar_alias:
         vsp_port_vios2_1 = "VSP6456_GB"
+        zone_vios2_fab1 = lpar_alias + "_" +vsp_port_vios2_1
+        return zone_vios2_fab1,vsp_port_vios2_1
+    elif "VIOLMP09_" in lpar_alias:
+        vsp_port_vios2_1 = "VSP6547_GB"
         zone_vios2_fab1 = lpar_alias + "_" +vsp_port_vios2_1
         return zone_vios2_fab1,vsp_port_vios2_1
 
@@ -96,4 +116,7 @@ def zonename4(lpar_alias):
         vsp_port_vios2_2 = "VSP6456_FD"
         zone_vios2_fab2 = lpar_alias + "_" +vsp_port_vios2_2
         return zone_vios2_fab2,vsp_port_vios2_2
-
+    if "VIOLMP09_" in lpar_alias:
+        vsp_port_vios2_2 = "VSP6547_FD"
+        zone_vios2_fab2 = lpar_alias + "_" +vsp_port_vios2_2
+        return zone_vios2_fab2,vsp_port_vios2_2
