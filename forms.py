c6cdc44c2ff097a95e59f9e6b4031729c7df7d16
angelmotta <angelmotta@gmail.com>
2016-09-09 17:26:44 -0500

    Version 2.0 Soporte para zonas pureflex
    Version 2.0 Soporte para zonas pureflex

--- forms.py
+++ forms.py
@@ -7,3 +7,10 @@ class wwnForm(Form):
     vios = SelectField('VIOS', choices=[('viochp11_12', 'VIOCHP11_12'), ('violmp11_12', 'VIOLMP11_12'), ('viochp08_09', 'VIOCHP08_09'), ('violmp08_09', 'VIOLMP08_09')])
     wwn = TextAreaField('Enter your wwns', [validators.Length(min=64, message="Enter at least 4 lines with your WWNs")])
     submit = SubmitField('Zoning')
+
+#Modificar por el nombre del PureFlex
+class wwnFormPure(Form):
+    host = TextField('Hostname Lpar', [validators.DataRequired("Please Enter the Server Name")])
+    vios = SelectField('VIOS', choices=[('pvioschp01', 'PVIOSCHP01'), ('pvioschd01', 'PVIOSCHD01'), ('pvioschd03', 'PVIOSCHD03'), ('pvioslmp01', 'PVIOSLMP01'), ('pvioslmd01', 'PVIOSLMD01'), ('pvioslmd03', 'PVIOSLMD03'), ('pvioslmd05', 'PVIOSLMD05')])
+    wwn = TextAreaField('Enter your wwns', [validators.Length(min=32, message="Enter at least 2 lines with your WWNs")])
+    submit = SubmitField('Zoning')
--- genzone.py
+++ genzone.py
@@ -14,6 +14,17 @@ def wwnparse(wwn):
     wwn4 = lista_wwn_result[3]
     return wwn1, wwn2, wwn3, wwn4
 
+def wwnparsepure(wwn):
+    lista_wwn = wwn.split()
+    lista_wwn_result = list()
+    for wwn_lpar in lista_wwn:
+        wwn = wwn_lpar[0:2]+":"+wwn_lpar[2:4]+":"+wwn_lpar[4:6]+":"+wwn_lpar[6:8]+":"+wwn_lpar[8:10]+":"+wwn_lpar[10:12]+":"+wwn_lpar[12:14]+":"+wwn_lpar[14:16]
+        lista_wwn_result.append(wwn)
+    wwn1 = lista_wwn_result[0]
+    wwn2 = lista_wwn_result[1]
+    return wwn1, wwn2
+
+# Generacion de Alias para POWER Lpars
 def lpar_alias(hostname, vios):
     hostname = hostname.upper()
     if vios == 'viochp11_12':
@@ -41,6 +52,39 @@ def lpar_alias(hostname, vios):
         lpar_alias4 = "VIOLMP09_" + hostname + "_FCSV1"
         return lpar_alias1, lpar_alias2, lpar_alias3, lpar_alias4
 
+# Generacion de Alias para PUREFLEX Lpars
+def alias_pure(hostname, vios):
+    hostname = hostname.upper()
+    if vios == 'pvioschp01':
+        lpar_alias1 = "PVIOSCHP01_" + hostname + "_FCSV0"
+        lpar_alias2 = "PVIOSCHP01_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2
+    if vios == 'pvioschd01':
+        lpar_alias1 = "PVIOSCHD01_" + hostname + "_FCSV0"
+        lpar_alias2 = "PVIOSCHD01_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2
+    if vios == 'pvioschd03':
+        lpar_alias1 = "PVIOSCHD03_" + hostname + "_FCSV0"
+        lpar_alias2 = "PVIOSCHD03_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2
+    if vios == 'pvioslmp01':
+        lpar_alias1 = "PVIOSLMP01_" + hostname + "_FCSV0"
+        lpar_alias2 = "PVIOSLMP01_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2
+    if vios == 'pvioslmd01':
+        lpar_alias1 = "PVIOSLMD01_" + hostname + "_FCSV0"
+        lpar_alias2 = "PVIOSLMD01_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2
+    if vios == 'pvioslmd03':
+        lpar_alias1 = "PVIOSLMD03_" + hostname + "_FCSV0"
+        lpar_alias2 = "PVIOSLMD03_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2
+    if vios == 'pvioslmd05':
+        lpar_alias1 = "PVIOSLMD05_" + hostname + "_FCSV0"
+        lpar_alias2 = "PVIOSLMD05_" + hostname + "_FCSV1"
+        return lpar_alias1, lpar_alias2
+
+## Generacion de ZONAS para Power
 def zonename1(lpar_alias):
     if "VIOCHP11_" in lpar_alias:
         vsp_port_vios1_1 = "VSP6456_1A"
@@ -120,3 +164,89 @@ def zonename4(lpar_alias):
         vsp_port_vios2_2 = "VSP6547_FD"
         zone_vios2_fab2 = lpar_alias + "_" +vsp_port_vios2_2
         return zone_vios2_fab2,vsp_port_vios2_2
+
+def zonepure1(lpar_alias):
+    if "PVIOSCHP01_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6456_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSCHD01_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSCHD03_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6456_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSCHD03_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSLMP01_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSLMD01_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSLMD03_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSLMD05_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+    elif "PVIOSLMD05_" in lpar_alias:
+        vsp_port_vios1_1 = "VSP6547_EB"
+        zone_vios1_fab1 = lpar_alias + "_" +vsp_port_vios1_1
+        fabric1 = "P_Fabric1"
+        return zone_vios1_fab1,vsp_port_vios1_1,fabric1
+
+
+def zonepure2(lpar_alias):
+    if "PVIOSCHP01_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6456_3D"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2            
+    elif "PVIOSCHD01_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6547_3D"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2            
+    elif "PVIOSCHD03_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6456_3D"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2            
+    elif "PVIOSLMP01_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6547_3D"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2 
+    elif "PVIOSLMD01_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6547_3D"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2 
+    elif "PVIOSLMD03_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6547_3D"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2 
+    elif "PVIOSLMD05_" in lpar_alias:
+        vsp_port_vios1_2 = "VSP6547_3D"
+        zone_vios1_fab2 = lpar_alias + "_" +vsp_port_vios1_2
+        fabric2 = "P_Fabric2"
+        return zone_vios1_fab2,vsp_port_vios1_2,fabric2 
+
